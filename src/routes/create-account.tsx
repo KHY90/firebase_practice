542074e70b1d83e7ab9b02538291f3ead31c0a7b
KHY90 <power4206@gmail.com>
2025-01-31 22:14:18 +0900

    로그인페에지, css분리리

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,49 +1,9 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
-import { styled } from "styled-components"
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
-
-const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 420px;
-    padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-    font-size: 42px;
-`;
-
-const Form = styled.form`
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100%;
-`;
-
-const Input = styled.input`
-    padding: 10px 20px;
-    border-radius: 50px;
-    border: none;
-    width: 100%;
-    font-size: 16px;
-    &[type="submit"]{
-        cursor: pointer;
-        &:hover{
-            opacity: 0.8;
-        }
-    }
-`;
-
-const Error = styled.span`
-    font-size: 600px;
-    color: tomato;
-`;
-
+import { useNavigate, Link, Form } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { Error, Input, Switcher, Title, Wrapper } from "../components/auth-components";
 
 export default function CreateAccount() {
     const navigate = useNavigate();
@@ -64,29 +24,39 @@ export default function CreateAccount() {
     };
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setError("");
         if (isLoading || name === "" || email === "" || password === "")
             return;
         try {
             const credentials = await createUserWithEmailAndPassword(auth, email, password);
-            console.log(credentials.user);
+            // console.log(credentials.user);
             await updateProfile(credentials.user, { displayName: name, });
             navigate("/");
         } catch (e) {
-
+            if (e instanceof FirebaseError) {
+                setError(e.message)
+            }
         } finally {
             setLoading(false);
         }
     };
 
-    return <Wrapper>
-        <Title>✖️ 회원 가입</Title>
-        <Form onSubmit={onSubmit}>
-            <Input onChange={onChange} name="name" value={name} placeholder="이름" type="text" required />
-            <Input onChange={onChange} name="email" value={email} placeholder="이메일" type="email" required />
-            <Input onChange={onChange} name="password" value={password} placeholder="비밀번호" type="password" required />
-            <Input type="submit" value={isLoading ? "Loading..." : "Create acoount"} />
-        </Form>
-        {error !== "" ? <Error>{error}</Error> : null}
-    </Wrapper>
-
+    return (
+        <Wrapper>
+            <Title>✖️ 회원 가입</Title>
+            <Form onSubmit={onSubmit}>
+                <Input onChange={onChange} name="name" value={name} placeholder="이름" type="text" required />
+                <Input onChange={onChange} name="email" value={email} placeholder="이메일" type="email" required />
+                <Input onChange={onChange} name="password" value={password} placeholder="비밀번호" type="password" required />
+                <Input type="submit" value={isLoading ? "Loading..." : "Create acoount"} />
+            </Form>
+            {error !== "" ? <Error>{error}</Error> : null}
+            <Switcher>
+                이미 계정이 있으신가요?{" "}
+                <Link to="/login">
+                    로그인 &rarr;
+                </ Link>
+            </Switcher>
+        </Wrapper>
+    );
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,57 @@
-export default function Login(){
-    return <h1>로그인</h1>
+import React, { useState } from "react";
+import { useNavigate, Link, Form } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { Error, Input, Switcher, Title, Wrapper } from "../components/auth-components";
+
+export default function CreateAccount() {
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("")
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { target: { name, value } } = e;
+        if (name === "password") {
+            setPassword(value)
+        } else if (name === "email") {
+            setEmail(value)
+        }
+    };
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+        if (isLoading || email === "" || password === "")
+            return;
+        try {
+            setLoading(true);
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/");
+        } catch (e) {
+            if(e instanceof FirebaseError){
+                setError(e.message)
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+    <Wrapper>
+        <Title>✖️ 로그인</Title>
+        <Form onSubmit={onSubmit}>
+            <Input onChange={onChange} name="email" value={email} placeholder="이메일" type="email" required />
+            <Input onChange={onChange} name="password" value={password} placeholder="비밀번호" type="password" required />
+            <Input type="submit" value={isLoading ? "Loading..." : "로그인"} />
+        </Form>
+        {error !== "" ? <Error>{error}</Error> : null}
+        <Switcher>
+            아직 계정이 없으신가요?{" "}
+             <Link to="/create-account">
+                Create &rarr;
+             </ Link>
+        </Switcher>
+    </Wrapper>
+    );
 }
